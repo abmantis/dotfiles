36d4cec11c28b0a7121ee701d92d056bf7d69eb5
abmantis <amfcalt@gmail.com>
2019-06-24 21:11:05 +0100

    ubuntu_updates -> system_updates (missing file)

---
+++ sway/.config/waybar/modules/system_updates.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+. /etc/os-release
+OS_NAME=$NAME
+
+HAS_UPDATES_ICON=
+UPDATE_COUNT=0
+REBOOT_REQUIRED=0
+
+if [[ "${OS_NAME}" = "Antergos Linux" ]]; then
+    TMPPATH="${TMPDIR:-/tmp}/checkup-db-${USER}"
+    DBPATH="$(pacman-conf DBPath)"
+
+    mkdir -p "$TMPPATH"
+    ln -s "$DBPATH/local" "$TMPPATH" &>/dev/null
+    fakeroot -- pacman -Sy --dbpath "$TMPPATH" --logfile /dev/null &>/dev/null
+    list=`pacman -Qu --dbpath "$TMPPATH" 2>/dev/null`
+
+    if [ "$list" != "" ]; then
+        UPDATE_COUNT=`echo "$list" | wc -l`
+    fi
+
+else    
+    # count how many updates we have got
+    UPDATE_COUNT=`/usr/lib/update-notifier/apt-check --human-readable | head -1 | awk '{print $1;}'`
+
+    if [ -f /var/run/reboot-required ]; then
+        echo -e "{\"text\":\""$HAS_UPDATES_ICON"\", \"class\":\"restart\", \"tooltip\":\"A system restart is required.\"}" 
+    elif [ "$UPDATE_COUNT" -gt "0" ]; then
+        echo -e "{\"text\":\""$HAS_UPDATES_ICON"\", \"class\":\"update\", \"tooltip\":\"There are $UPDATE_COUNT updates\"}"
+    else
+        echo -e "{\"text\":\"\", \"class\":\"uptodate\"}"
+    fi
+fi
+
+if [[ $REBOOT_REQUIRED = 1 ]]; then
+    echo -e "{\"text\":\""$HAS_UPDATES_ICON"\", \"class\":\"restart\", \"tooltip\":\"A system restart is required.\"}" 
+elif [ "$UPDATE_COUNT" -gt "0" ]; then
+    echo -e "{\"text\":\""$HAS_UPDATES_ICON"\", \"class\":\"update\", \"tooltip\":\"There are $UPDATE_COUNT updates\"}"
+else
+    echo -e "{\"text\":\"\", \"class\":\"uptodate\"}"
+fi
