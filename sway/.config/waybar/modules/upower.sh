92e455cee4f69bb365d7e519720fb813c2109b4b
abmantis <amfcalt@gmail.com>
2020-05-03 15:35:44 +0100

    improve waybar power module

--- sway/.config/waybar/modules/upower.sh
+++ sway/.config/waybar/modules/upower.sh
@@ -5,25 +5,23 @@ DISPLAYDEVICE=$(upower -e | grep 'DisplayDevice')
 UPOWER_DATA=$(upower -i ${DISPLAYDEVICE})
 BATTERY_STATE=$(echo "${UPOWER_DATA}" | grep -E "state|to\ full" | awk '{print $2}')
 BATTERY_POWER=$(echo "${UPOWER_DATA}" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
-BATTERY_TIME=$(echo "${UPOWER_DATA}" | grep -E "time\ to\ empty" | awk '{print $4}{print $5}')
-URGENT_VALUE=10
+BATTERY_TIME=$(echo "${UPOWER_DATA}" | grep -E "time\ to\ empty" | awk '{print $4 " " $5}')
 
 printf -v BATTERY_POWER_INT %.0f "$BATTERY_POWER"
 
-if [[ "${BATTERY_POWER_INT}" -gt 87 ]]; then
+if [[ "${BATTERY_POWER_INT}" -gt 90 ]]; then
     BATTERY_ICON=""
-elif [[ "${BATTERY_POWER_INT}" -gt 63 ]]; then
+elif [[ "${BATTERY_POWER_INT}" -gt 60 ]]; then
      BATTERY_ICON=""
-elif [[ "${BATTERY_POWER_INT}" -gt 38 ]]; then
+elif [[ "${BATTERY_POWER_INT}" -gt 40 ]]; then
      BATTERY_ICON=""
-elif [[ "${BATTERY_POWER_INT}" -gt 13 ]]; then
+elif [[ "${BATTERY_POWER_INT}" -gt 15 ]]; then
      BATTERY_ICON=""
-elif [[ "${BATTERY_POWER_INT}" -le 13 ]]; then
+elif [[ "${BATTERY_POWER_INT}" -le 15 ]]; then
      BATTERY_ICON=""
 else
     BATTERY_ICON=""
 fi
-
 
 if [[ "${BATTERY_STATE}" != "discharging" ]]; then
     BATTERY_ICON=""
@@ -31,8 +29,14 @@ fi
 
 if [[ "${BATTERY_POWER_INT}" -le 15 ]]; then
     CLASS="low"
+    TEXT="$BATTERY_ICON $BATTERY_POWER_INT%"
+elif [[ "${BATTERY_POWER_INT}" -le 65 ]]; then
+    CLASS="mid"
+    TEXT="$BATTERY_ICON $BATTERY_POWER_INT%"
 else
-    CLASS="normal"
+    CLASS="high"
+    TEXT="$BATTERY_ICON"
 fi
 
-echo -e "{\"text\":\""$BATTERY_ICON $BATTERY_POWER_INT%"\", \"class\":\""$CLASS"\", \"tooltip\":\"Remaining: "$BATTERY_TIME"\"}"
+printf '{"text":"%s","tooltip":"Remaining: %s%% (%s)","class":"%s"}\n' \
+			"$TEXT" "$BATTERY_POWER_INT" "$BATTERY_TIME"  "$CLASS"
