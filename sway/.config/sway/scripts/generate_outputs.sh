fbb177d313c1faa37bd44dc00bd50a54ef3003b7
Abilio Costa <abilio.costa@criticaltechworks.com>
2020-09-03 17:58:05 +0100

    Improve generate_outputs.sh

--- sway/.config/sway/scripts/generate_outputs.sh
+++ sway/.config/sway/scripts/generate_outputs.sh
@@ -7,75 +7,102 @@ OUTPUTS_GEN_FILE=$HOME/.config/sway/enabled_configs/outputs_gen.conf
 # Clean file
 echo '' > $OUTPUTS_GEN_FILE
 
+function write_line {
+    echo $1 >> $OUTPUTS_GEN_FILE
+}  
+
 CURR_OUTPUTS_LIST=$(swaymsg -t get_outputs | jq -r '.[] | "\(.make) \(.model) \(.serial)"')
+CURR_OUTPUT_COUNT=$(swaymsg -t get_outputs | jq -r '.[] | "\(.make) \(.model) \(.serial)"' | wc -l)
 
-if grep -Fxq "Ancor Communications Inc ASUS ML239 BBLMIB518036" <<< $CURR_OUTPUTS_LIST
-then
-    # Setup outputs
-    echo '
-        output eDP-1 scale 1
-        output eDP-1 pos 0 0
-        output "Ancor Communications Inc ASUS ML239 BBLMIB518036" pos 1920 0
-        ' \
-    >> $OUTPUTS_GEN_FILE
+OUTPUT_LAPTOP="eDP-1"
+OUTPUT_E1=""
+OUTPUT_E2=""
 
-    # Assign outputs for each workspace
-    echo '
-        set $wsbsoutput eDP-1
-        set $ws1output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws2output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws3output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws4output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws5output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws6output  "Ancor Communications Inc ASUS ML239 BBLMIB518036" 
-        set $ws7output  eDP-1
-        set $ws8output  eDP-1
-        set $ws9output  eDP-1
-        set $ws10output eDP-1
-        ' \
-    >> $OUTPUTS_GEN_FILE
-elif grep -Fxq "Dell Inc. DELL U2417H 5K9YD83VB68L" <<< $CURR_OUTPUTS_LIST
-then
-    # Setup outputs
-    echo '
-        output eDP-1 pos 0 0
-        output "Dell Inc. DELL U2419H 611KRS2" pos 1920 0
-        output "Dell Inc. DELL U2417H 5K9YD83VB68L" pos 3840 0
-        ' \
-    >> $OUTPUTS_GEN_FILE
+ASUS_MODEL="Ancor Communications Inc ASUS ML239 BBLMIB518036"
+DELL1_MODEL="Dell Inc. DELL U2419H 611KRS2"
+DELL2_MODEL="Dell Inc. DELL U2417H 5K9YD83VB68L"
 
-    # Assign outputs for each workspace
-    echo '
-        set $wsbsoutput eDP-1
-        set $ws1output  "Dell Inc. DELL U2419H 611KRS2"
-        set $ws2output  "Dell Inc. DELL U2417H 5K9YD83VB68L"
-        set $ws3output  "Dell Inc. DELL U2419H 611KRS2"
-        set $ws4output  "Dell Inc. DELL U2419H 611KRS2"
-        set $ws5output  "Dell Inc. DELL U2419H 611KRS2"
-        set $ws6output  "Dell Inc. DELL U2419H 611KRS2"
-        set $ws7output  "Dell Inc. DELL U2417H 5K9YD83VB68L"
-        set $ws8output  "Dell Inc. DELL U2417H 5K9YD83VB68L"
-        set $ws9output  "Dell Inc. DELL U2417H 5K9YD83VB68L"
-        set $ws10output eDP-1
-        ' \
-    >> $OUTPUTS_GEN_FILE
-else
-    # Assign outputs for each workspace
-    echo '
-        set $wsbsoutput eDP-1
-        set $ws1output  eDP-1
-        set $ws2output  eDP-1
-        set $ws3output  eDP-1
-        set $ws4output  eDP-1
-        set $ws5output  eDP-1
-        set $ws6output  eDP-1
-        set $ws7output  eDP-1
-        set $ws8output  eDP-1
-        set $ws9output  eDP-1
-        set $ws10output eDP-1
-        ' \
-    >> $OUTPUTS_GEN_FILE
+if grep -Fxq "$ASUS_MODEL" <<< $CURR_OUTPUTS_LIST; then
+    OUTPUT_E1=$ASUS_MODEL
+
+elif grep -Fxq "$DELL1_MODEL" <<< $CURR_OUTPUTS_LIST; then
+    OUTPUT_E1=$DELL1_MODEL
+    
+    if grep -Fxq "$DELL2_MODEL" <<< $CURR_OUTPUTS_LIST; then
+        OUTPUT_E2=$DELL2_MODEL
+    fi
+
+elif grep -Fxq "$DELL2_MODEL" <<< $CURR_OUTPUTS_LIST; then
+    OUTPUT_E1=$DELL2_MODEL
 fi
+
+############################################
+# Setup outputs
+write_line "output \"$OUTPUT_LAPTOP\" scale 1"
+write_line "output \"$OUTPUT_LAPTOP\" pos 0 0"
+
+if [ ! -z "$OUTPUT_E1" ]; then
+    write_line "output \"$OUTPUT_E1\" pos 1920 0"
+fi
+if [ ! -z "$OUTPUT_E2" ]; then
+    write_line "output \"$OUTPUT_E2\" pos 3840 0"
+fi
+
+############################################
+# Setup workspace assignments
+
+WSBSOUTPUT=$OUTPUT_LAPTOP
+WS1OUTPUT=$OUTPUT_LAPTOP
+WS2OUTPUT=$OUTPUT_LAPTOP
+WS3OUTPUT=$OUTPUT_LAPTOP
+WS4OUTPUT=$OUTPUT_LAPTOP
+WS5OUTPUT=$OUTPUT_LAPTOP
+WS6OUTPUT=$OUTPUT_LAPTOP
+WS7OUTPUT=$OUTPUT_LAPTOP
+WS8OUTPUT=$OUTPUT_LAPTOP
+WS9OUTPUT=$OUTPUT_LAPTOP
+WS10OUTPUT=$OUTPUT_LAPTOP
+
+if [ ! -z "$OUTPUT_E1" ]; then
+    if [ ! -z "$OUTPUT_E2" ]; then
+        WSBSOUTPUT=$OUTPUT_LAPTOP
+        WS1OUTPUT=$OUTPUT_E1
+        WS2OUTPUT=$OUTPUT_E2
+        WS3OUTPUT=$OUTPUT_E1
+        WS4OUTPUT=$OUTPUT_E1
+        WS5OUTPUT=$OUTPUT_E1
+        WS6OUTPUT=$OUTPUT_E1
+        WS7OUTPUT=$OUTPUT_E2
+        WS8OUTPUT=$OUTPUT_E2
+        WS9OUTPUT=$OUTPUT_E2
+        WS10OUTPUT=$OUTPUT_LAPTOP
+    else
+        WSBSOUTPUT=$OUTPUT_LAPTOP
+        WS1OUTPUT=$OUTPUT_E1
+        WS2OUTPUT=$OUTPUT_E1
+        WS3OUTPUT=$OUTPUT_E1
+        WS4OUTPUT=$OUTPUT_E1
+        WS5OUTPUT=$OUTPUT_E1
+        WS6OUTPUT=$OUTPUT_E1
+        WS7OUTPUT=$OUTPUT_E1
+        WS8OUTPUT=$OUTPUT_E1
+        WS9OUTPUT=$OUTPUT_E1
+        WS10OUTPUT=$OUTPUT_LAPTOP
+    fi
+
+fi
+
+write_line "set \$wsbsoutput \"$WSBSOUTPUT\""
+write_line "set \$ws1output  \"$WS1OUTPUT\""
+write_line "set \$ws2output  \"$WS2OUTPUT\""
+write_line "set \$ws3output  \"$WS3OUTPUT\""
+write_line "set \$ws4output  \"$WS4OUTPUT\""
+write_line "set \$ws5output  \"$WS5OUTPUT\""
+write_line "set \$ws6output  \"$WS6OUTPUT\""
+write_line "set \$ws7output  \"$WS7OUTPUT\""
+write_line "set \$ws8output  \"$WS8OUTPUT\""
+write_line "set \$ws9output  \"$WS9OUTPUT\""
+write_line "set \$ws10output \"$WS10OUTPUT\""
 
 # reload sway
 swaymsg reload
