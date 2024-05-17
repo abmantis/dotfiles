74161391ec009d30b4aec29ff83f375fb44ed06e
Abilio Costa <abilio.costa@criticaltechworks.com>
2024-05-17 08:03:13 +0100

    Fix lock on suspend; add calendar

--- hypr/scripts/idle_handler.sh
+++ hypr/scripts/idle_handler.sh
@@ -3,10 +3,11 @@
 locktime=$1
 screenofftime=$2
 lockcmd=$3
+presleepcmd=$4
 
 let notifytime=$locktime-5 
-swayidle -w \
+swayidle \
     timeout $locktime "$lockcmd" \
     timeout $screenofftime 'hyprctl dispatch dpms off' \
         resume 'hyprctl dispatch dpms on' \
-    before-sleep "$lockcmd"
+    before-sleep "$presleepcmd"
