28fba0c1708f09434a9b2fc9a8dd1e4991eb2108
Abilio Costa <abilio.costa@criticaltechworks.com>
2024-06-06 01:39:15 +0100

    Add new sway config

---
+++ sway/scripts/idle_handler.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+locktime=$1
+screenofftime=$2
+lockcmd=$3
+presleepcmd=$4
+
+let notifytime=$locktime-5 
+swayidle -w \
+    timeout $locktime "$lockcmd" \
+    timeout $screenofftime 'hyprctl dispatch dpms off' \
+        resume 'hyprctl dispatch dpms on' \
+    before-sleep "swaylock -f --indicator-radius 50 --indicator-thickness 6 --bs-hl-color=b48eadff --caps-lock-bs-hl-color=d08770ff --caps-lock-key-hl-color=ebcb8bff --inside-color=2e3440ff --inside-clear-color=81a1c1ff --inside-ver-color=5e81acff --inside-wrong-color=bf616aff --key-hl-color=a3be8cff --layout-bg-color=2e3440ff --line-uses-ring  --ring-color=3b4252ff --ring-clear-color=88c0d0ff --ring-ver-color=81a1c1ff --ring-ver-color=81a1c1ff --separator-color=3b4252ff --text-color=eceff4ff --text-clear-color=3b4252ff --text-ver-color=3b4252ff --text-wrong-color=3b4252ff"
---
+++ waybar/modules/upower.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+BATTERY=0
+DISPLAYDEVICE=$(upower -e | grep 'DisplayDevice')
+UPOWER_DATA=$(upower -i ${DISPLAYDEVICE})
+BATTERY_STATE=$(echo "${UPOWER_DATA}" | grep -E "state|to full" | awk '{print $2}')
+BATTERY_POWER=$(echo "${UPOWER_DATA}" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
+BATTERY_TIME=$(echo "${UPOWER_DATA}" | grep -E "time to empty" | awk '{print $4 " " $5}')
+
+printf -v BATTERY_POWER_INT %.0f "$BATTERY_POWER"
+
+if [[ "${BATTERY_POWER_INT}" -gt 80 ]]; then
+    BATTERY_ICON=""
+elif [[ "${BATTERY_POWER_INT}" -gt 60 ]]; then
+     BATTERY_ICON=""
+elif [[ "${BATTERY_POWER_INT}" -gt 40 ]]; then
+     BATTERY_ICON=""
+elif [[ "${BATTERY_POWER_INT}" -gt 20 ]]; then
+     BATTERY_ICON=""
+else
+     BATTERY_ICON=""
+fi
+
+if [[ "${BATTERY_STATE}" != "discharging" ]]; then
+    BATTERY_ICON=""
+fi
+
+if [[ "${BATTERY_POWER_INT}" -le 15 ]]; then
+    CLASS="low"
+    TEXT="$BATTERY_ICON $BATTERY_POWER_INT%"
+elif [[ "${BATTERY_POWER_INT}" -le 65 ]]; then
+    CLASS="mid"
+    TEXT="$BATTERY_ICON $BATTERY_POWER_INT%"
+else
+    CLASS="high"
+    TEXT="$BATTERY_ICON"
+fi
+
+printf '{"text":"%s","tooltip":"Remaining: %s%% (%s)","class":"%s"}\n' \
+			"$TEXT" "$BATTERY_POWER_INT" "$BATTERY_TIME"  "$CLASS"
