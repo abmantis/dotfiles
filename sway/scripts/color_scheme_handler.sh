8b74f4304f18124b4f7a754650aa4a8977908ef6
abmantis <amfcalt@gmail.com>
2024-11-28 17:25:02 +0000

    Add workaround for kitty theme change

--- sway/scripts/color_scheme_handler.sh
+++ sway/scripts/color_scheme_handler.sh
@@ -6,12 +6,14 @@ set_nvim () {
 
 set_light () {
     kitty +kitten themes --config-file-name theme.conf --reload-in=all Edge-light &
+    pkill -SIGUSR1 kitty
     find $XDG_RUNTIME_DIR -name "nvim.*" 2>/dev/null | while read file; do set_nvim "$file" "light"; done &
     find /tmp/nvim.$USER/ -type s 2>/dev/null | while read file; do set_nvim "$file" "light"; done &
 }
 
 set_dark () {
     kitty +kitten themes --config-file-name theme.conf --reload-in=all Edge-neon &
+    pkill -SIGUSR1 kitty
     find $XDG_RUNTIME_DIR -name "nvim.*" 2>/dev/null | while read file; do set_nvim "$file" "dark"; done &
     find /tmp/nvim.$USER/ -type s 2>/dev/null | while read file; do set_nvim "$file" "dark"; done &
 }
