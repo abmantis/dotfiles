d74908a2b45460411e8167e5a90947d3e52b9dd7
abmantis <amfcalt@gmail.com>
2026-04-23 17:54:04 +0100

    Add keybinding to split workspace in 3

---
+++ niri/scripts/niri_tripple_view.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+niri msg action focus-column-first && \
+niri msg action set-column-width "33.333%" && \
+niri msg action focus-column-right && \
+niri msg action set-column-width "33.333%" && \
+niri msg action focus-column-right && \
+niri msg action set-column-width "33.333%" && \
+niri msg action focus-column-left && \
+niri msg action focus-column-left 
+
